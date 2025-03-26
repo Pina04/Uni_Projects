ef927545e7c8406857d3f4e7300978cc177b3f5c
Pina <101382965+Pina04@users.noreply.github.com>
2025-03-26 12:56:12 -0300

    Add files via upload
    arithmetic operations and classifications with python

---
+++ Aula_1 Operadores Aritimeticos.py
@@ -0,0 +1,25 @@
+num1 = 5
+num2 = 2
+
+print(type(num1), type(num2))
+
+som = num1 + num2
+print("Som:", som, type(som))
+
+sub = num1 - num2
+print("Sub:", sub, type(sub))
+
+mult = num1 * num2
+print("Mult:", mult, type(mult))
+
+div = num1 / num2
+print("Div:", div, type(div))
+
+pot = num1 ** num2
+print("Pot:", pot, type(pot))
+
+div_int = num1 // num2
+print("Div_int:", div_int, type(div_int))
+
+rest = num1 % num2
+print("Rest:", rest, type(rest))
