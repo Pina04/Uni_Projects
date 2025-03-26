029b2c158f53e6f823e988a83c5b54d03a4028b6
Pina <101382965+Pina04@users.noreply.github.com>
2025-03-26 12:57:19 -0300

    Add files via upload
    first teach about python on fiap

---
+++ PythonAulasPCP_01.py
@@ -0,0 +1,28 @@
+print("Hello, World!")
+
+print(7+4)
+
+print("7" + "4" )
+
+#Comentários em python!!!!
+'''
+Da pra escrever
+em várias linhas com
+esse tipo de comentário
+'''
+
+
+#Váriaves
+
+#nome recebe "Alexandre"
+nome = "Alexandre"
+idade = 25
+peso = 65
+
+print(nome, idade, peso)
+
+nome = input("Qual é o seu nome?")
+#input = colocar a respostas no terminal
+
+print(nome)
+
